8d63fb1d944a3a2c4a911c9f5cad43aafc719f32
xiongod <xiongod@outlook.com>
2024-05-17 03:02:44 +0000

    Adding mp3 via GitHub Actions

---
+++ js/data.js
@@ -0,0 +1,344 @@
+[
+    {
+        "title": "25年前发生在东京地铁最令人难忘的事件（下）一个至今让日本人后怕的疯狂组织[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/c8a0c695-5c1d-41d3-8a62-d5042668ed3b.mp3",
+        "duration": "14分26秒",
+        "published": ""
+    },
+    {
+        "title": "25年前发生在东京地铁最令人难忘的事件（上）一个至今让日本人后怕的疯狂组织[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/90ecce9e-2bf5-44a2-9268-c86607fce95a.mp3",
+        "duration": "14分36秒",
+        "published": ""
+    },
+    {
+        "title": "支配整个宇宙的神奇数字法国路易十六国王一生难逃的诅咒马航MH17千丝万缕的联系[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/f0fc8cb3-30ee-4caa-8dc3-ffca3a76c26a.mp3",
+        "duration": "11分51秒",
+        "published": ""
+    },
+    {
+        "title": "美国死亡谷地下神秘都市疑是亚特兰蒂斯遗迹？巨人族是传说还是史实？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/b84f182c-765d-447e-b7b0-6148047b436b.mp3",
+        "duration": "13分29秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】谜团重重涂满指甲油的双手真想究竟是。。？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/42f83d05-5cb3-4508-92d5-eedf0f0f19f3.mp3",
+        "duration": "20分53秒",
+        "published": ""
+    },
+    {
+        "title": "【悬疑案】美国黑色大丽花迷案70年未破悬案二十多岁妙龄美女遇害背后的真实故事[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/1224862f-ed06-4ca8-bb1b-22308708508a.mp3",
+        "duration": "15分14秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】英国闹市街区最安全保险库令人大跌眼镜的一幕几个男子趁着黑夜...[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/448e9f62-54ab-4c49-8d46-e08c8f77427b.mp3",
+        "duration": "13分22秒",
+        "published": ""
+    },
+    {
+        "title": "人体冷冻后复活的真实案例我们离长生不老真的不远了吗？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/302581ed-5731-4eea-98a4-21fa903609a4.mp3",
+        "duration": "12分45秒",
+        "published": ""
+    },
+    {
+        "title": "[小乌说案]韩国闻庆十字架奇案是对信仰的执着？还是另有隐情？[脑洞乌托邦|小烏TV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/51f19ed1-5c76-4e90-a95b-58776e3888ae.mp3",
+        "duration": "14分55秒",
+        "published": ""
+    },
+    {
+        "title": "从未来穿越回来的天才达芬奇追踪消失的名画安吉里之战比特斯拉更牛的男人[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/b82eaad9-fb2c-400e-816d-130cfb512457.mp3",
+        "duration": "12分43秒",
+        "published": ""
+    },
+    {
+        "title": "西方版笔仙碟仙秘鲁中学学生集体中邪有关通灵板的都市传说[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/59bfdb5e-f44e-479e-aca8-9ea4dd188b2d.mp3",
+        "duration": "12分28秒",
+        "published": ""
+    },
+    {
+        "title": "巴黎景区禁地里拍摄到的诡异影片神秘墓穴里到底有什么？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/04e58026-85bb-47c4-ac10-7f331b2ca668.mp3",
+        "duration": "15分16秒",
+        "published": ""
+    },
+    {
+        "title": "现实比剧情更残酷张紫妍胜利门郑俊英事件真相能否浮出水面？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/305a178c-d94f-4f0a-b2d3-524c7025a81e.mp3",
+        "duration": "22分10秒",
+        "published": ""
+    },
+    {
+        "title": "史上最黑暗心理学实验正常人变为恶魔需要几天？斯坦福监狱实验[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/f86dba19-fe17-4094-a0ff-7e5de36bde5a.mp3",
+        "duration": "14分18秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】至今成谜的“呼兰大侠”建国后人人闻风丧胆的第一大案揭秘真实的呼兰大侠[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/184f1563-3d2a-4814-9cd4-532fe82360dc.mp3",
+        "duration": "23分21秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】震惊一时的五大洋事件揭秘韩国政界商界的黑暗史[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/bb908ae2-2c3b-4efe-9c7e-d55e98eefaaa.mp3",
+        "duration": "17分18秒",
+        "published": ""
+    },
+    {
+        "title": "\"世越號\"惨案(下)5年前那艘船上究竟发生了什么？永远不该被遗忘的痛[脑洞乌托邦|MysteryStoriesTV][脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/7ff8e57d-e8eb-4103-89b9-02b90b110d8a.mp3",
+        "duration": "17分2秒",
+        "published": ""
+    },
+    {
+        "title": "\"世越號\"(上)5年前那艘船上究竟发生了什么？永远不该被遗忘的痛[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/cc9979ac-4f8f-4852-81dc-e7b23be9e302.mp3",
+        "duration": "13分46秒",
+        "published": ""
+    },
+    {
+        "title": "【悬疑案】夫妻外出旅游高速路上的离奇意外真相只有一个神探李昌钰[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/92b63e00-e7d5-4b6c-9cd0-fed5acee50df.mp3",
+        "duration": "12分16秒",
+        "published": ""
+    },
+    {
+        "title": "【悬疑案】日本7年未结悬案电台灵异事件高速路上的意外是天灾还是人祸？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/f0ab30f3-c64a-42c4-8f13-1b52dc4097d9.mp3",
+        "duration": "14分10秒",
+        "published": ""
+    },
+    {
+        "title": "世界各国的人鱼传说古代的人鱼和现代的人鱼有什么区别？美人鱼到底是不是真实存在的？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/b4187024-002b-4ff0-a7c8-efa8aa1f13f6.mp3",
+        "duration": "10分38秒",
+        "published": ""
+    },
+    {
+        "title": "【悬疑案】震惊中外的太平洋大逃杀（下）出海33人归来11人一艘渔船上发生的真实故事[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/049058bb-b843-440a-bc64-3c5cefbd51a6.mp3",
+        "duration": "14分35秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】震惊中外的太平洋大案（上）出海33人归来11人一艘渔船上发生的真实故事[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/85cee0ba-58e4-45b3-a14e-2417f0d3aeaa.mp3",
+        "duration": "16分29秒",
+        "published": ""
+    },
+    {
+        "title": "中国古代第一禁书鲁班书木匠指南怎么就和法术有关？为什么说得罪谁都不要得罪木匠？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/9a159a37-720c-4bc2-b142-aa5036b0ed1d.mp3",
+        "duration": "10分38秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】济州岛年轻少妇神秘消失是失足落水还是另有原因？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/1bef9fb5-f24d-46e6-9fb4-13eb06139c7e.mp3",
+        "duration": "15分0秒",
+        "published": ""
+    },
+    {
+        "title": "尼比鲁地球殖民史不用苦苦寻找神的遗迹了我们都是神的后代地球编年史[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/1f28384b-324b-471d-ba71-c31273eb0693.mp3",
+        "duration": "15分54秒",
+        "published": ""
+    },
+    {
+        "title": "大洪水的中西方记载到底是传说还是史实？生存下来的我们都是被神选定的人[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/78378c10-4644-4956-b586-c432abdcdd56.mp3",
+        "duration": "12分17秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】李亨浩事件韩国电影【那家伙的声音】中的真实故事60多通电话留下了什么线索？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/3df24bae-8f0b-4013-9da8-540a84ee466d.mp3",
+        "duration": "18分39秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】一个由可乐引发的连环事件[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/7932a10c-0590-49c6-8cd6-4102ab014ade.mp3",
+        "duration": "14分16秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】韩国版越狱续集风流大盗辛昌元[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/273282ca-ba51-4c0c-920f-7eb9a818f1f8.mp3",
+        "duration": "8分3秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】韩国三大未结案件青蛙少年失踪案(上)[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/6137ee65-9035-441e-ba76-6d4a1c84b6e3.mp3",
+        "duration": "9分41秒",
+        "published": ""
+    },
+    {
+        "title": "神秘消失的西域古国为什么丝绸之路的枢纽国一夜之间被风沙掩埋？聊聊楼兰消失之谜[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/04cee284-f2f3-4ab4-9e6a-30f1eed4a22c.mp3",
+        "duration": "10分2秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】美女离奇得怪病20多年为何真相难破？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/bffb5628-e64b-4766-b74b-cf7a310a500a.mp3",
+        "duration": "14分12秒",
+        "published": ""
+    },
+    {
+        "title": "时间旅行真的可行吗？穿越时空的两种方法霍金的时间旅行者宴会在将来是否有人会来？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/cb90e5a8-e0a8-479a-902b-670c77c972a9.mp3",
+        "duration": "8分43秒",
+        "published": ""
+    },
+    {
+        "title": "人类为什么要探索宇宙？星际穿越中隐含的深意[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/3fdace32-3153-467c-b409-250df816ed46.mp3",
+        "duration": "10分36秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】建国后[第一大案]离奇消失的村民们真相只有一个[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/b80c2f5e-070b-4416-b694-bb8129dda66b.mp3",
+        "duration": "13分58秒",
+        "published": ""
+    },
+    {
+        "title": "秘密太空计划人类正在面临重大转型科里古德揭秘如何开天目和启动超能力[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/4478431c-1df5-4950-bd6a-47dfc5c601d7.mp3",
+        "duration": "13分34秒",
+        "published": ""
+    },
+    {
+        "title": "中国古墓发现2000年不朽木乃伊东方睡美人背后隐藏着什么秘密？考古探秘志[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/4079a03e-85f0-4785-ab4d-56043da1ece5.mp3",
+        "duration": "8分59秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】韩国釜山新婚夫妇密室中凭空消失的俩个人到底去了哪儿？结局反转再反转[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/12d9fe52-d034-4e2d-8948-d603767c7559.mp3",
+        "duration": "16分21秒",
+        "published": ""
+    },
+    {
+        "title": "流浪地球如果成为现实太阳耗尽人类永夜下的命运我们还能活多久？8分钟倒计时是否成真？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/e8121115-d4fe-4e02-993f-8eb9e15386d7.mp3",
+        "duration": "6分43秒",
+        "published": ""
+    },
+    {
+        "title": "南极冰川下的真相雪地金字塔和冰下大峡谷难道是古文明的遗迹？AntarcticaGreatCanyon[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/f0f657b0-d494-4b78-b9af-6e2ce040799e.mp3",
+        "duration": "8分27秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】印度离奇案件家中日记本说出隐藏的真相[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/5f86a992-c23b-42df-b973-529f230aa148.mp3",
+        "duration": "10分29秒",
+        "published": ""
+    },
+    {
+        "title": "被美国中情局认证的中国特异功能第一人探索火星过去CIA星门计划特异功能超能力研究项目Stargate[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/68259a3c-502b-41ce-87ee-c47192f17d6e.mp3",
+        "duration": "11分26秒",
+        "published": ""
+    },
+    {
+        "title": "中国发现巨大金字塔比胡夫金字塔还壮观与埃及金字塔存在着何等诡异巧合？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/8b8ff9ed-1836-4263-9923-f3f10bd549c1.mp3",
+        "duration": "7分41秒",
+        "published": ""
+    },
+    {
+        "title": "能够改变世界却带着惊天秘密离开的科学家们尼古拉特斯拉[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/6cb74bb6-b368-4656-b7db-f0b02075d649.mp3",
+        "duration": "11分27秒",
+        "published": ""
+    },
+    {
+        "title": "中国无人区罗布泊神秘事件失踪的飞机行踪不明的中国科学家[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/6337e578-2360-4ffc-bd26-2b27e1beb2bb.mp3",
+        "duration": "11分11秒",
+        "published": ""
+    },
+    {
+        "title": "超级地球,人类可以搬家了?火星移民计划太阳系内的宜居星球[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/764e7926-7ee0-4123-9143-c892425161ac.mp3",
+        "duration": "7分16秒",
+        "published": ""
+    },
+    {
+        "title": "辛普森卡通神预言调侃中美政府难道我们真的被操控着？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/259825d5-147b-447d-bc3e-21421c7ab81a.mp3",
+        "duration": "12分12秒",
+        "published": ""
+    },
+    {
+        "title": "外星人为什么不来找我们？费米悖论的可能解释Aliens&FermiParadox[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/e86e98c3-2f31-420f-9904-2b0af1122a35.mp3",
+        "duration": "6分21秒",
+        "published": ""
+    },
+    {
+        "title": "密宗的双修西藏喇嘛教禁秘话题藏传佛教那些你不知道的事[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/4f32b28e-d309-48af-8b47-2c873919a604.mp3",
+        "duration": "10分19秒",
+        "published": ""
+    },
+    {
+        "title": "中国嫦娥4号成功等陆月球背面外星人存在的4个证据伊卡ICA石史前核反应堆百慕大三角洲Chang‘e-4landing[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/ce4abd7f-9622-4a2d-8214-b2729c111ae7.mp3",
+        "duration": "8分19秒",
+        "published": ""
+    },
+    {
+        "title": "2019跨年悉尼烟花秀全球最美丽的烟火秀SydneyFireworks大家的新年愿望都是什么呀？[脑洞乌托邦|MysteryTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/5d53aafe-b51d-48c7-92df-ab01af3cdeae.mp3",
+        "duration": "12分5秒",
+        "published": ""
+    },
+    {
+        "title": "2019年10大预言中国经济泡沫破裂中国房市岌岌可危下一届的美国总统是谁？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/b1943319-13b3-4fd8-93c4-0ba5b11bfebf.mp3",
+        "duration": "7分4秒",
+        "published": ""
+    },
+    {
+        "title": "CCTVGHOST真实拍到的灵异视频世界上真的有鬼吗？最后一个太。。。[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/aea051ac-73f0-493d-95d3-5810063e4343.mp3",
+        "duration": "8分48秒",
+        "published": ""
+    },
+    {
+        "title": "【小乌说案】现实版越狱他是如何从犯人变成国名英雄的？韩版风流大盗楚留香[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/280b771a-bc73-4d1a-9062-37eeab280bf4.mp3",
+        "duration": "6分35秒",
+        "published": ""
+    },
+    {
+        "title": "前蘇聯不可思議實驗三一只狗狗可以有兩個頭移植了其他人的頭會發生什麽呢？[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/aed4b812-8c5d-4b25-9936-dbb21d30b7c9.mp3",
+        "duration": "7分44秒",
+        "published": ""
+    },
+    {
+        "title": "人类和大猩猩的基因会融合出什么样的后代？前苏联不可思议实验二[脑洞乌托邦|MysteryStoriesTV]",
+        "url": "https://xiongod.github.io/ndwtb2019/data/5d395a0e-960f-4f4c-be8f-fb24b623e73d.mp3",
+        "duration": "8分16秒",
+        "published": ""
+    }
+]
